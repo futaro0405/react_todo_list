94e997aaaf60373936e4f2f0924d0e2191dc7938
github futaro0405 <fkswtkm0405.web@gmail.com>
2023-12-10 13:45:02 +0900

    update stylelint settings

--- stylelintrc.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  plugins: ['stylelint-scss', 'stylelint-order'],
-  extends: ['stylelint-config-standard', 'stylelint-prettier/recommended'],
-  rules: {
-    'order/properties-alphabetical-order': true,
-    'at-rule-no-unknown': null,
-    'scss/at-rule-no-unknown': true
-  }
-};
