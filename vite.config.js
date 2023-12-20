54139adcd6a1e40b5f85166d7d1c198faa489f79
github futaro0405 <fkswtkm0405.web@gmail.com>
2023-12-20 20:49:33 +0900

    update github actions

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,29 @@ import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/react_todo_list/',
+  root: 'src',
+  publicDir: '../public',
+  build: {
+    outDir: '../dist',
+    emptyOutDir: true,
+    rollupOptions: {
+      output: {
+        entryFileNames: `[name].js`,
+        chunkFileNames: `modules/[name].js`,
+        assetFileNames: (assetInfo) => {
+          if (/\.( gif|jpeg|jpg|png|svg|webp| )$/.test(assetInfo.name)) {
+            return 'assets/images/[name].[ext]';
+          }
+
+          if (/\.css$/.test(assetInfo.name)) {
+            return 'assets/css/[name].[ext]';
+          }
+
+          return 'assets/[name].[ext]';
+        }
+      }
+    }
+  },
   plugins: [react({ jsxImportSource: '@emotion/react' })]
 });
